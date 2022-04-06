3541f14c4fda494fafba1a9b5251c54790683796
Yuvraj Singh <32823553+ysrdevs@users.noreply.github.com>
2022-04-06 04:02:53 -0500

    Initial commit

---
+++ lib/addresses.ts
@@ -0,0 +1,8 @@
+import { PublicKey } from "@solana/web3.js"
+
+// this is your shop wallet address
+export const shopAddress = new PublicKey('F27c1pHDUUunwGQUprCdiuBmJCdSNHrBpF24WWLDCLb6')
+// this is the same for everyone!
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+// This is your token/coupon address
+export const couponAddress = new PublicKey('AruuJMsDw3WPXn9gmv9mEq5ZKu89Pz49fnSqWj2YBGhG')
---
+++ lib/products.ts
@@ -0,0 +1,18 @@
+export const products = [
+  {
+    id: 'box-of-cookies',
+    name: 'Phantom Sour Candies',
+    description: 'A delicious bag of our famouus Sour Candies',
+    unitName: 'Bags', // shows after the price, eg. 0.05 SOL/bag
+    priceSol: 0.05,
+    priceUsd: 5,
+  },
+  {
+    id: 'basket-of-cookies',
+    name: 'Pack',
+    description: 'A large pack of 12 Phantom Sour Candy bags',
+    unitName: 'basket',
+    priceSol: 0.5,
+    priceUsd: 50,
+  }
+]
